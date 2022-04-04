a93a387f57ce52a77ca7b9f99359ca308853375d
czt <2315096491@qq.com>
2022-04-04 19:19:31 +0800

    update raft/raft.go

--- code/raft/raft.go
+++ code/raft/raft.go
@@ -1,22 +1,5 @@
 package raft
 
-//
-// this is an outline of the API that raft must expose to
-// the service (or tester). see comments below for
-// each of these functions for more details.
-//
-// rf = Make(...)
-//   create a new Raft server.
-// rf.Start(command interface{}) (index, term, isleader)
-//   start agreement on a new log entry
-// rf.GetState() (term, isLeader)
-//   ask a Raft for its current term, and whether it thinks it is leader
-// ApplyMsg
-//   each time a new entry is committed to the log, each Raft peer
-//   should send an ApplyMsg to the service (or tester)
-//   in the same server.
-//
-
 import (
 	"824/labgob"
 	"bytes"
@@ -33,17 +16,6 @@ import (
 	"824/labrpc"
 )
 
-//
-// as each Raft peer becomes aware that successive log entries are
-// committed, the peer should send an ApplyMsg to the service (or
-// tester) on the same server, via the applyCh passed to Make(). set
-// CommandValid to true to indicate that the ApplyMsg contains a newly
-// committed log entry.
-//
-// in part 2D you'll want to send other kinds of messages (e.g.,
-// snapshots) on the applyCh, but set CommandValid to false for these
-// other uses.
-//
 type ApplyMsg struct {
 	CommandValid bool
 	Command      interface{}
@@ -62,15 +34,12 @@ type Entry struct {
 	Term    int
 }
 
-//
-// A Go object implementing a single Raft peer.
-//
 type Raft struct {
-	mu        sync.Mutex          // Lock to protect shared access to this peer's state
-	peers     []*labrpc.ClientEnd // RPC end points of all peers
-	persister *Persister          // Object to hold this peer's persisted state
-	me        int                 // this peer's index into peers[]
-	dead      int32               // set by Kill()
+	mu        sync.Mutex          
+	peers     []*labrpc.ClientEnd 
+	persister *Persister          
+	me        int                 
+	dead      int32               
 
 	applyCh chan ApplyMsg
 
@@ -79,8 +48,6 @@ type Raft struct {
 	// 可在 crash 后零初始化的字段, 参考论文 Figure 2, 另外还要考虑 leader 和心跳机制
 }
 
-// return currentTerm and whether this server
-// believes it is the leader.
 func (rf *Raft) GetState() (int, bool) {
 
 	// 返回 curTerm 和 isLeader, 注意线程安全
@@ -93,20 +60,12 @@ func (rf *Raft) encodeState() []byte {
 
 }
 
-//
-// save Raft's persistent state to stable storage,
-// where it can later be retrieved after a crash and restart.
-// see paper's Figure 2 for a description of what should be persistent.
-//
 func (rf *Raft) persist() {
 
 	// 使用 rf.persister.SaveStateAndSnapshot 保存状态和快照信息
 
 }
 
-//
-// restore previously persisted state.
-//
 func (rf *Raft) readPersist(state []byte, snapshot []byte) {
 	if state == nil || len(state) < 1 { // bootstrap without any state?
 		return
@@ -123,24 +82,12 @@ func (rf *Raft) readPersist(state []byte, snapshot []byte) {
 
 }
 
-// When a service falls far behind the leader and must catch up,
-// the service first installs a snapshot and then replays log entries
-// from after the point at which the snapshot was created.
-
-//
-// A service wants to switch to snapshot.  Only do so if Raft hasn't
-// have more recent info since it communicate the snapshot on applyCh.
-//
 func (rf *Raft) CondInstallSnapshot(lastIncludedTerm int, lastIncludedIndex int, snapshot []byte) bool {
 
 	// 按上方注解要求, 当 lastApplied 和第 2 个参数相等时返回 true
 
 }
 
-// the service says it has created a snapshot that has
-// all info up to and including index. this means the
-// service no longer needs the log through (and including)
-// that index. Raft should now trim its log as much as possible.
 func (rf *Raft) Snapshot(index int, snapshot []byte) {
 
 	// 开启一个 goroutine, 生成快照, 否则会跟 cfg 里的 apply 循环形成死锁
@@ -153,9 +100,6 @@ func (rf *Raft) makeSnapshot(index int, snapshot []byte) {
 
 }
 
-//
-// RequestVote RPC handler.
-//
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 
 	// 设置一个持久化标志, 并用 defer 检查
@@ -184,9 +128,6 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 
 }
 
-//
-// AppendEntries RPC handler
-//
 func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) {
 
 	// 设置一个持久化标志, 并用 defer 检查
@@ -243,20 +184,6 @@ func (rf *Raft) InstallSnapshot(args *InstallSnapshotArgs, reply *InstallSnapsho
 
 }
 
-//
-// the service using Raft (e.g. a k/v server) wants to start
-// agreement on the next command to be appended to Raft's log. if this
-// server isn't the leader, returns false. otherwise, start the
-// agreement and return immediately. there is no guarantee that this
-// command will ever be committed to the Raft log, since the leader
-// may fail or lose an election. even if the Raft instance has been killed,
-// this function should return gracefully.
-//
-// the first return value is the index that the command will appear at
-// if it's ever committed. the second return value is the current
-// term. the third return value is true if this server believes it is
-// the leader.
-//
 func (rf *Raft) Start(command interface{}) (int, int, bool) {
 
 	// index :=
@@ -282,17 +209,6 @@ func (rf *Raft) applyTicker() {
 	}
 }
 
-//
-// the tester doesn't halt goroutines created by Raft after each test,
-// but it does call the Kill() method. your code can use killed() to
-// check whether Kill() has been called. the use of atomic avoids the
-// need for a lock.
-//
-// the issue is that long-running goroutines use memory and may chew
-// up CPU time, perhaps causing later tests to fail and generating
-// confusing debug output. any goroutine with a long-running loop
-// should call killed() to check whether it should stop.
-//
 func (rf *Raft) Kill() {
 	atomic.StoreInt32(&rf.dead, 1)
 	// Your code here, if desired.
@@ -303,8 +219,6 @@ func (rf *Raft) killed() bool {
 	return z == 1
 }
 
-// The ticker go routine starts a new election if this peer hasn't received
-// heartsbeats recently.
 func (rf *Raft) ticker() {
 
 	// 根据当前节点的编号进行 rand.Seed()
@@ -322,18 +236,6 @@ func (rf *Raft) ticker() {
 	}
 }
 
-//
-//   the service or tester wants to create a Raft server. the ports
-// of all the Raft servers (including this one) are in peers[]. this
-// server's port is peers[me]. all the servers' peers[] arrays
-// have the same order.
-//   persister is a place for this server to save its persistent state,
-// and also initially holds the most recent saved state, if any.
-//   applyCh is a channel on which the tester or service expects Raft
-// to send ApplyMsg messages.
-//   Make() must return quickly, so it should start goroutines
-// for any long-running work.
-//
 func Make(peers []*labrpc.ClientEnd, me int,
 	persister *Persister, applyCh chan ApplyMsg) *Raft {
 	rf := &Raft{}
